da958544b64eb848c9e8b84492618f7e6a343e71
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-04 22:13:25 -0800

    Update dataproduct.py

--- dataproduct.py
+++ dataproduct.py
@@ -952,6 +952,7 @@ def page7():
     if st.button('Predict Rent Price'):
         price = predict(htype, dogs, smoking, wheelchair, electric, furnished, bedbath, laundry, parking, state)
         st.write("Predicted Rent Price")
+        st.write("$", price)
         st.success(np.exp(price).astype(int))
         
     
