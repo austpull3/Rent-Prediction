faeeaa55d54053458516ebc790d98ddd95239638
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-04 23:12:13 -0800

    Update dataproduct.py

--- dataproduct.py
+++ dataproduct.py
@@ -100,19 +100,19 @@ def main_page():
     st.markdown("# ENJOY!")
     st.sidebar.markdown("# Welcome!🎈❄️")
     
-        st.sidebar.write("Use the widgets to alter the graphs:")
-        chck = st.sidebar.checkbox("Use your theme colours on graphs", value=True) # get colours for graphs
+    st.sidebar.write("Use the widgets to alter the graphs:")
+    chck = st.sidebar.checkbox("Use your theme colours on graphs", value=True) # get colours for graphs
 
-        # get colors from theme config file, or set the colours to altair standards
-        if chck:
-            primary_clr = st.get_option("theme.primaryColor")
-            txt_clr = st.get_option("theme.textColor")
-            # I want 3 colours to graph, so this is a red that matches the theme:
-            second_clr = "#d87c7c"
-        else:
-            primary_clr = '#4c78a8'
-            second_clr = '#f58517'
-            txt_clr = '#e45756'
+    # get colors from theme config file, or set the colours to altair standards
+    if chck:
+        primary_clr = st.get_option("theme.primaryColor")
+        txt_clr = st.get_option("theme.textColor")
+         # I want 3 colours to graph, so this is a red that matches the theme:
+        second_clr = "#d87c7c"
+    else:
+        primary_clr = '#4c78a8'
+        second_clr = '#f58517'
+        txt_clr = '#e45756'
 
 
 def page2():
