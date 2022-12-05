fe54e21faf1fec3bc76f64b234438680c7ec1265
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-04 23:42:13 -0800

    Update dataproduct.py

--- dataproduct.py
+++ dataproduct.py
@@ -31,6 +31,8 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 st.set_page_config(layout="wide", page_icon=":house:", page_title="Rent Prediction")
+st.get_option("theme.font")
+
 st.write("Streamlit version", st.__version__)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
