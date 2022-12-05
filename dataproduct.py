2802e98fa370b479754f085531ed80410ba429fa
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-04 23:47:36 -0800

    Update dataproduct.py

--- dataproduct.py
+++ dataproduct.py
@@ -31,8 +31,8 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 st.set_page_config(layout="wide", page_icon=":house:", page_title="Rent Prediction")
-st.get_option("theme.font")
-st.get_option('main/Rent-Prediction/.streamlit/config.toml/theme.textColor')
+color = st.color_picker("Pick a color', '#00f900')
+
 
 st.write("Streamlit version", st.__version__)
 st.set_option('deprecation.showPyplotGlobalUse', False)
