9d4c6e1b7a1179c8b12429bd6b4e74e628a270d6
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-05 00:05:38 -0800

    Update dataproduct.py

--- dataproduct.py
+++ dataproduct.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
+import seaborn as sns 
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
